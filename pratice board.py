d9a78a1f21726a8c9a3e9b020bd8cba665af6950
jay4baba <66269245+jay4baba@users.noreply.github.com>
2021-04-17 17:23:46 +0100

    Add files via upload

---
+++ pratice board.py
@@ -0,0 +1,26 @@
+class budget():
+
+	def __init__ (self, category, amount):
+		self.category = category
+		self.amount = amount
+
+		def deposit(self):
+			return "new amount + amount"
+
+		def check_balance(self):
+			return "new deposit + amount"
+
+		def withdraw(self):
+			return "amount - amount to withdraw"
+
+		def transfer(self):
+			return "amount to transfer"
+
+category1 = {'meal': 3000}
+category2 = {'Netflix': 1000}
+category3 = {'Transport': 2000}
+
+print(category.deposit)
+
+
+	
